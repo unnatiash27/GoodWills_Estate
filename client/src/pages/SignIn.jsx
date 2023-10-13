5d2b3c0cdc07c6dabe19925631321a5c1cdb051b
unnatiash27 <unnatiash27@gmail.com>
2023-10-13 10:25:44 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,53 +1,52 @@
 import { useState } from "react";
-import{Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
-  
   const [formData, setFormData] = useState({});
-  const[error,setError] = useState(null);
-  const[loading,setLoading] = useState(false);
+  const { loading,error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const handleChange= (e) =>{
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]:e.target.value,
+      [e.target.id]: e.target.value,
     });
   };
 
-  const handleSubmit=async(e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      const res = await fetch('/api/auth/signin' ,
-      {
-        method: 'POST',
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-Type' : 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(data.success === false) {
-        setError(data.message);
-        setLoading(false);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
-      } 
-      setLoading(false);
-      setError(null);
-      navigate('/');
+      }
+      dispatch(signInSuccess(data));
+      navigate("/");
       console.log(data);
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
-      
-    }    
+      dispatch(signInFailure(error.message));
+    }
   };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-        
         <input
           type="email"
           placeholder="Email"
@@ -62,8 +61,11 @@ export default function SignIn() {
           id="password"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85">
-          {loading? 'Loading...' : 'Sign In'}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85"
+        >
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 my-5 ">
@@ -72,7 +74,7 @@ export default function SignIn() {
           <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
