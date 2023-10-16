4168ba2adac60a40028afa32675806080681a0f5
unnatiash27 <unnatiash27@gmail.com>
2023-10-16 08:44:57 +0530

    update header

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,34 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { app } from '../firebase';
+
+export default function OAuth() {
+
+    const handleGoogleClick = async () =>{
+        try {
+            const provider = new GoogleAuthProvider();
+            const auth = getAuth(app);
+      
+            const result = await signInWithPopup(auth, provider);
+
+            const res = await fetch('/api/auth/google', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  name: result.user.displayName,
+                  email: result.user.email,
+                  photo: result.user.photoURL,
+                }),
+              });
+        } catch (error) {
+            console.log('could not sign in with google', error);
+        }
+    }
+
+  return (
+    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85'>
+      Continue with Google
+    </button>
+  );
+}
