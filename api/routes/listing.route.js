1aad18909ed6369b3abef8bd1a2eb403c37d0927
unnatiash27 <unnatiash27@gmail.com>
2024-01-08 18:52:12 +0530

    created listing pg UI

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -7,4 +7,4 @@ const router = express.Router();
 
 router.post('/create', verifyToken ,createListing)
 
-export default router;
+export default router; 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import Createlisting from './pages/Createlisting';
 
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route element={<PrivateRoute/>} >
         <Route path='/profile' element={<Profile />} />
+        <Route path='/create-listing' element={<Createlisting />} />
         </Route>
 
       </Routes>
