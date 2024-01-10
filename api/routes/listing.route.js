eddeea635f1a70d16f70f2f7f9f0f1c4bac0aae1
unnatiash27 <unnatiash27@gmail.com>
2024-01-10 11:23:29 +0530

    created get user listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,9 @@
-import express from "express";
+import express from 'express';
 import { createListing } from '../controllers/listing.controller.js';
-//import { verify } from "jsonwebtoken";
-import { verifyToken } from "../utils/verifyUser.js";
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.post('/create', verifyToken ,createListing)
+router.post('/create', verifyToken, createListing);
 
-export default router; 
+export default router;
