ae4c34fc06324fbef986a83e8776236e4d566c1a
unnatiash27 <unnatiash27@gmail.com>
2024-07-09 12:05:05 +0530

    Added contact landlord functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -137,11 +137,14 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact && (
-              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+            {/*{currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
                 Contact landlord
               </button>
             )}
+            {contact && <Contact listing={listing}/>} */}
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+            <button onClick={() => setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Contact Landlord</button>)}
             {contact && <Contact listing={listing}/>}
           </div>
         </div>
