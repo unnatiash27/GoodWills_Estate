b095960c00bf77f4e2308ca9893cb022edae128c
unnatiash27 <unnatiash27@gmail.com>
2024-07-10 11:49:59 +0530

    vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import { inject } from '@vercel/analytics';
+inject();
 
 export default function App() {
   return (
