e6ca3f4e309a5d224333c819958553413814b645
unnatiash27 <unnatiash27@gmail.com>
2024-07-19 11:03:18 +0530

    t2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,6 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
-
 export default function App() {
   return (
     <BrowserRouter>
@@ -22,7 +21,6 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
-
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
